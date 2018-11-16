1b626ef5c2ffc772f5d56a3f6d3e177c081f4f29
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2018-11-16 21:07:04 +0100

    proper leaflet setup with vue js single file components

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <l-map :zoom="zoom" :center="center">
+    <button @click="panToLatLng">OK</button>
+    <l-map style="height:200px;" ref="map" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <l-marker :lat-lng="marker"></l-marker>
     </l-map>
@@ -8,11 +9,17 @@
 </template>
 
 <script>
-import mymap from "vue2-leaflet";
+import Vue2Leaflet from "vue2-leaflet";
+//var { LMap, LTileLayer, LMarker } = Vue2Leaflet;
+//after babel transpiling (probably called destructering)
+//var _Vue2Leaflet = Vue2Leaflet,
+//LMap = _Vue2Leaflet.LMap,
+//LTileLayer = _Vue2Leaflet.LTileLayer,
+//LMarker = _Vue2Leaflet.LMarker;
 
-console.log(mymap);
+console.log(Vue2Leaflet);
 
-var { LMap, LTileLayer, LMarker } = mymap;
+var { L, LMap, LTileLayer, LMarker } = Vue2Leaflet;
 
 export default {
   name: "MyMap",
@@ -20,12 +27,17 @@ export default {
   data() {
     return {
       zoom: 8,
-      center: [53.0, 13.5], //L.latLng(47.41322, -1.219482),
+      center: L.latLng(53.0, 13.5), //[53.0, 13.5], //L.latLng(47.41322, -1.219482),
       url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: [53.0, 13.5] //L.latLng(47.41322, -1.219482)
+      marker: L.latLng(47.41322, -1.219482)
     };
+  },
+  methods: {
+    panToLatLng: function(event) {
+      console.log(this.$refs.map.mapObject);
+    }
   }
 };
 </script>
