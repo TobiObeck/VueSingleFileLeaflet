8580af088b5972181002487f13a86006bf360d48
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2018-11-16 20:23:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld /> -->
+    <MyCoolMap />
+  </div>
+</template>
+
+<script>
+//import HelloWorld from "./components/HelloWorld";
+import MyCoolMap from "./components/MyMap";
+
+export default {
+  name: "App",
+  components: {
+    //HelloWorld,
+    MyCoolMap
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyMap.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <l-map :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="marker"></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import mymap from "vue2-leaflet";
+
+console.log(mymap);
+
+var { LMap, LTileLayer, LMarker } = mymap;
+
+export default {
+  name: "MyMap",
+  components: { LMap, LTileLayer, LMarker },
+  data() {
+    return {
+      zoom: 8,
+      center: [53.0, 13.5], //L.latLng(47.41322, -1.219482),
+      url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: [53.0, 13.5] //L.latLng(47.41322, -1.219482)
+    };
+  }
+};
+</script>
+<style></style>
