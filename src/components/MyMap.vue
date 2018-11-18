2b77436f9cd9333a8a2ff694969780401a0636a4
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2018-11-18 22:03:51 +0100

    requesting dummy data

--- src/components/MyMap.vue
+++ src/components/MyMap.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <button @click="requestData">Get data</button>
     <input
       v-model="searchInput"
       @keydown.enter="requestCoordsBySearchInput"
@@ -10,8 +11,29 @@
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <l-marker :lat-lng="marker"></l-marker>
     </l-map>
-    DEBUG: coords {{ searchedLat }}, {{ searchedLong }} Lädt?
-    {{ this.isCalculating }} Fehler? {{ this.isError }}
+    <span v-show="true"
+      >DEBUG: coords {{ searchedLat }}, {{ searchedLong }} Lädt?
+
+      {{ this.isCalculating }} Fehler? {{ this.isError }} <br
+    /></span>
+    <ul>
+      <li v-show="index < 5" v-for="(coord, index) in datacoords" :key="index">
+        {{ index + 1 }}: {{ coord.lat }}, {{ coord.long }}
+      </li>
+      <li>{{ Math.min(datacoords.length + 1, 6) }}: ...</li>
+    </ul>
+    <table id="coordstable">
+      <tr>
+        <th>No.</th>
+        <th>Latitude</th>
+        <th>Longitude</th>
+      </tr>
+      <tr v-for="(coord, index) in datacoords" :key="index">
+        <td>{{ index + 1 }}</td>
+        <td>{{ coord.lat }}</td>
+        <td>{{ coord.long }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -35,6 +57,7 @@ export default {
   components: { LMap, LTileLayer, LMarker },
   data() {
     return {
+      datacoords: [{ lat: 0, long: 0 }],
       searchedLat: 0,
       searchedLong: 0,
       searchInputIsDirty: false,
@@ -55,6 +78,21 @@ export default {
     }
   },
   methods: {
+    requestData() {
+      this.isCalculating = true;
+      axios
+        .get(
+          "https://my-json-server.typicode.com/tobiobi/myjasonserver/coordinates"
+        )
+        .then(response => {
+          this.datacoords = response.data;
+        })
+        .catch(error => {
+          this.isError = true;
+          console.log(error);
+        })
+        .finally(() => (this.isCalculating = false));
+    },
     requestCoordsBySearchInput: debounce(function() {
       this.isCalculating = true;
       console.log("searchInput", this.searchInput);
@@ -71,9 +109,22 @@ export default {
           console.log(error);
         })
         .finally(() => (this.isCalculating = false));
-    }, 500)
+    }, 500),
+    addPushpinWithPopup(lat, long, message) {
+      var tempMap = this.$refs.map.mapObject;
+      L.marker([lat, long])
+        .addTo(tempMap)
+        .bindPopup(message)
+        .openPopup();
+    }
   },
   watch: {
+    datacoords: function() {
+      for (var i = 0; i < this.datacoords.length; i++) {
+        var tempCoord = this.datacoords[i];
+        this.addPushpinWithPopup(tempCoord.lat, tempCoord.long, "foo:" + i);
+      }
+    },
     searchInput: function() {
       this.searchInputIsDirty = true;
       this.requestCoordsBySearchInput();
@@ -85,21 +136,44 @@ export default {
         animate: true,
         duration: 5
       });
-      L.marker([this.searchedLat, this.searchedLong])
-        .addTo(tempMap)
-        .bindPopup(
-          "Standort: " +
-            this.searchInput +
-            " (" +
-            this.searchedLat +
-            ", " +
-            this.searchedLong +
-            ")"
-        )
-        .openPopup();
+      var popupMessage = "Location: ";
+      popupMessage += this.searchInput;
+      popupMessage += " (" + this.searchedLat + ", " + this.searchedLong + ")";
+      this.addPushpinWithPopup(
+        this.searchedLat,
+        this.searchedLong,
+        popupMessage
+      );
       L.circle([52.5, 13.5], { radius: 10000 }).addTo(tempMap); // radius in meters
     }
   }
 };
 </script>
-<style></style>
+<style>
+#coordstable {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#coordstable td,
+#coordstable th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+#coordstable tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+#coordstable tr:hover {
+  background-color: #ddd;
+}
+
+#coordstable th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4caf50;
+  color: white;
+}
+</style>
